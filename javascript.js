276a575c3ab4da5b04d717ca1025f662babbd85f
Colin Cason <colinjcason@gmail.com>
2019-07-30 07:01:15 -0700

    populating gifs to html

--- javascript.js
+++ javascript.js
@@ -1,13 +1,35 @@
-// declare variables for the ajax call
-var APIKey = "cbhe5AwSsFySg4FYmflgWQ12Ww7qse4M";
+// array that will be generated into buttons
+var topics = ["The Office", "Friends", "Parks & Recreation", "Michael Scott", "Ron Swanson", "Will Ferrell", "Step Brothers"];    
+var APIKey = "cbhe5AwSsFySg4FYmflgWQ12Ww7qse4M"
+    
+// function is called when form is submitted
+    $("#find-gif").on("click", function() {
 
-        var xhr = $.get("https://api.giphy.com/v1/gifs/search?&api_key=cbhe5AwSsFySg4FYmflgWQ12Ww7qse4M&limit=5");
-        xhr.done(function(data) { console.log("success got data", data); });
+        $("#image-dump").empty();
 
-        $.ajax({
-            url: xhr,
-            method: "GET"
-        }).then(function(data) {
-            console.log(data);
-        });
+    // prevent submit button from submitting a request
+    event.preventDefault();
+
+    var input = $("#gif-input").val();
+
+    // variable to hold API key
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=" + APIKey + "&limit=10");
+
+    xhr.done(function(response) { 
+        
+        console.log("success got data", response);
+
+        var results = response.data;
+
+        for(var i = 0; i < results.length; i++) {
+            var imageURL = results[i].images.fixed_height.url;
+            var imageDiv = $("<img>");
+            imageDiv.attr("src", imageURL);
+            $("#image-dump").prepend(imageDiv);
+        }
+    
+    });
+
+});
+
 
